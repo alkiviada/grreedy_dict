55ae4fc783ba345e2df2321342104838f4152d69
anastasia senenko <ana@anastasias-MacBook-Pro.local>
2019-06-26 08:37:43 -0400

    font work: optimization of font loading; converting icons in font awesome into something lighter

--- frontend/src/components/Word.js
+++ frontend/src/components/Word.js
@@ -4,12 +4,22 @@ import key from "weak-key";
 
 const Word = (props) => {
   const { wordElement, visibilityFilter, visibility, deleteWord } = props;
+  const wl =  wordElement[1].length
+  const l = 8.2 * wl + 18
+  console.log(l)
   return ( 
     <div className="word" key={key(wordElement)}>
-    <strong className="just-word">{wordElement[1]}</strong>
-    { visibility == 'hide' ? <a className="word-show fas fa-arrow-down" onClick={(e) => visibilityFilter(e, wordElement[1])}></a> :
-    <a className="word-hide fas fa-arrow-up" onClick={(e) => visibilityFilter(e, wordElement[1])}></a> }
-    <a className="word-delete fas fa-window-close" onClick={(e) => deleteWord(e, wordElement[1])}></a>
+    <svg className="word-svg"   
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      viewBox={"0 0 " + l + " 25"} preserveAspectRatio="xMidYMid meet">
+      <text x="0" y="17" className="word-svg-text">
+    {wordElement[1]}
+      </text>
+     </svg>
+    { visibility == 'hide' ? <button className="word-show" onClick={(e) => visibilityFilter(e, wordElement[1])}>></button> :
+    <button className="word-hide" onClick={(e) => visibilityFilter(e, wordElement[1])}>></button> }
+    <button className="word-delete" onClick={(e) => deleteWord(e, wordElement[1])}>x</button>
     </div>
   );
 }
